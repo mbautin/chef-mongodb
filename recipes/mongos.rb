9bbe4476dd685d4377b704c019d9493c87d2b7c3
Mikhail Bautin <mbautin@gmail.com>
2013-12-15 01:27:36 -0800

    Better error message when an incorrect number of config servers is found

--- recipes/mongos.rb
+++ recipes/mongos.rb
@@ -34,7 +34,8 @@ configsrv = search(
 )
 
 if configsrv.length != 1 and configsrv.length != 3
-  Chef::Log.error("Found #{configsrv.length} configserver, need either one or three of them")
+  Chef::Log.error("Found #{configsrv.length} configserver, need either one or three of them, " +
+                  "but found #{configsrv.length}: #{configsrv}")
   raise "Wrong number of configserver nodes"
 end
 
